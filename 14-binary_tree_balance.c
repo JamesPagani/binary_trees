9ee035c924cf7f93f8457af354b77ac1eaa9d34a
JamesPagani <jaimeagalvezv@gmail.com>
2020-03-13 08:18:23 +0000

    Using the optimized height algorithm

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,47 +1,5 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_leaf - Checks if the given node is a leaf (has no child)
- * @node: Node to check
- * Return: 1 if @node is a leaf, 0 if otherwise or @node is NULL
- */
-
-int binary_tree_is_leaf(const binary_tree_t *node)
-{
-	if (node == NULL)
-		return (0);
-	if (!(node->left) && !(node->right))
-		return (1);
-	return (0);
-}
-
-
-/**
- * calc_depth - Calculates the depth of a binary tree
- * @tree: Binary tree to measure
- * @depth: Depth of the tree or sub-tree
- * Return: @depth
- */
-
-size_t calc_depth(const binary_tree_t *tree, size_t depth)
-{
-	size_t left_d = depth;
-	size_t right_d = depth;
-
-	if (!(binary_tree_is_leaf(tree)))
-	{
-		if (tree->left)
-			left_d = calc_depth(tree->left, depth + 1);
-		if (tree->right)
-			right_d = calc_depth(tree->right, depth + 1);
-		if (left_d > right_d)
-			return (left_d);
-		else
-			return (right_d);
-	}
-	return (depth);
-}
-
-/**
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Binary tree to measure
  * Description: Checks the distance (in nodes) from @tree to the deepest node
@@ -50,9 +8,20 @@ size_t calc_depth(const binary_tree_t *tree, size_t depth)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_h = 0;
+	size_t right_h = 0;
+
 	if (tree == NULL)
 		return (0);
-	return (calc_depth(tree, 0));
+
+	if (tree->left)
+		left_h = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_h = 1 + binary_tree_height(tree->right);
+
+	if (left_h > right_h)
+		return (left_h);
+	return (right_h);
 }
 
 /**
